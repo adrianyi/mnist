63cd631eba797491a7d55d8bd2fe6c3589a8a105
Adrian Yi <sjay87@gmail.com>
2018-10-25 00:11:04 -0700

    Format fixes. Add script to install OpenMPI/Horovod and run

--- mnist_horovod.py
+++ mnist_horovod.py
@@ -1,4 +1,3 @@
-import json
 import os
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 
@@ -26,7 +25,9 @@ print('task_index:', task_index)
 print('ps_hosts:', ps_hosts)
 print('worker_hosts:', worker_hosts)
 
+
 def str2bool(v):
+    """"""
     if v.lower() in ('yes', 'true', 't', 'y', '1'):
         return True
     elif v.lower() in ('no', 'false', 'f', 'n', '0'):
@@ -34,8 +35,9 @@ def str2bool(v):
     else:
         raise IOError('Boolean value expected (i.e. yes/no, true/false, y/n, t/f, 1/0).')
 
+
 def get_args():
-    '''Return parsed args'''
+    """Return parsed args"""
     parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
     parser.add_argument('--local_data_dir', type=str, default='data/',
                         help='Path to local data directory')
@@ -47,7 +49,7 @@ def get_args():
     # Model params
     parser.add_argument('--hidden_units', type=int, nargs='*', default=[32, 64])
     parser.add_argument('--learning_rate', type=float, default=0.001)
-    parser.add_argument('--learning_decay', type=float, default=0.001)
+    parser.add_argument('--learning_decay', type=float, default=0.05)
     parser.add_argument('--dropout', type=float, default=0.1)
     parser.add_argument('--batch_size', type=int, default=512)
 
@@ -56,16 +58,17 @@ def get_args():
 
     opts = parser.parse_args()
 
-    opts.data_dir = get_data_path(dataset_name = 'adrianyi/mnist-data',
-                                 local_root = opts.local_data_dir,
-                                 local_repo = '',
-                                 path = '')
-    opts.log_dir = get_logs_path(root = opts.local_log_dir)
+    opts.data_dir = get_data_path(dataset_name='adrianyi/mnist-data',
+                                  local_root=opts.local_data_dir,
+                                  local_repo='',
+                                  path='')
+    opts.log_dir = get_logs_path(root=opts.local_log_dir)
 
     return opts
 
+
 class IteratorInitializerHook(tf.train.SessionRunHook):
-    '''From https://medium.com/onfido-tech/higher-level-apis-in-tensorflow-67bfb602e6c0'''
+    """From https://medium.com/onfido-tech/higher-level-apis-in-tensorflow-67bfb602e6c0"""
     def __init__(self):
         super(IteratorInitializerHook, self).__init__()
         self.iterator_initializer_func = None
@@ -73,15 +76,17 @@ class IteratorInitializerHook(tf.train.SessionRunHook):
     def after_create_session(self, session, coord):
         self.iterator_initializer_func(session)
 
-def cnn_net(input_tensor):
-    '''Return logits output from CNN net'''
+
+def cnn_net(input_tensor, opts):
+    """Return logits output from CNN net"""
     temp = tf.reshape(input_tensor, shape=(-1, 28, 28, 1), name='input_image')
     for i, n_units in enumerate(opts.hidden_units):
         temp = tf.layers.conv2d(temp, filters=n_units, kernel_size=(3, 3), strides=(2, 2),
                                 activation=tf.nn.relu, name='cnn'+str(i))
         temp = tf.layers.dropout(temp, rate=opts.dropout)
-    temp = tf.reduce_mean(temp, axis=(2,3), keepdims=False, name='average')
+    temp = tf.reduce_mean(temp, axis=(2, 3), keepdims=False, name='average')
     return tf.layers.dense(temp, 10)
+
 
 def main(opts):
     # Initiate Horovod
@@ -89,6 +94,7 @@ def main(opts):
     print('Horovod size:', hvd.size())
     print('Horovod local rank:', hvd.local_rank())
     print('Horovod rank:', hvd.rank())
+    is_chief = (hvd.rank() == 0)
 
     if opts.fashion:
         data = read_data_sets(opts.data_dir,
@@ -97,9 +103,13 @@ def main(opts):
         data = read_data_sets(opts.data_dir)
 
     def model_fn(features, labels, mode):
-        logits = cnn_net(features)
-        loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(labels=labels, logits=logits, name='cross_entropy'), name='loss')
-        lr = tf.train.exponential_decay(learning_rate=opts.learning_rate*hvd.size(), global_step=tf.train.get_global_step(), decay_steps=1, decay_rate=opts.learning_decay)
+        logits = cnn_net(features, opts)
+        cent = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=labels, logits=logits, name='cross_entropy')
+        loss = tf.reduce_mean(cent, name='loss')
+        lr = tf.train.exponential_decay(learning_rate=opts.learning_rate*hvd.size(),
+                                        global_step=tf.train.get_global_step(),
+                                        decay_steps=1000,
+                                        decay_rate=1.-opts.learning_decay)
         optimizer = tf.train.AdamOptimizer(learning_rate=lr)
         # Horovod distributed optimizer
         optimizer = hvd.DistributedOptimizer(optimizer)
@@ -116,18 +126,18 @@ def main(opts):
                                           eval_metric_ops=metrics)
 
     session_config = tf.ConfigProto()
-    session_config.gpu_options.allow_growth = True # pylint: no-member
-    session_config.gpu_options.visible_device_list = str(hvd.local_rank()) #pylint: disable=no-member
+    session_config.gpu_options.allow_growth = True
+    session_config.gpu_options.visible_device_list = str(hvd.local_rank())
 
     runconfig = tf.estimator.RunConfig(
-        model_dir=(opts.log_dir if hvd.rank() == 0 else None),
-        save_summary_steps=(50 if hvd.rank() == 0 else None),
-        save_checkpoints_steps=50,
+        model_dir=(opts.log_dir if is_chief else None),
+        save_summary_steps=(25 if is_chief else None),
+        save_checkpoints_steps=25,
         keep_checkpoint_max=2,
-        log_step_count_steps=(10 if hvd.rank() == 0 else None),
+        log_step_count_steps=(10 if is_chief else None),
         session_config=session_config)
     estimator = tf.estimator.Estimator(
-        model_dir=(opts.log_dir if hvd.rank() == 0 else None),
+        model_dir=(opts.log_dir if is_chief else None),
         model_fn=model_fn,
         config=runconfig)
     bcast_hook = hvd.BroadcastGlobalVariablesHook(0)
@@ -137,7 +147,9 @@ def main(opts):
                          y=data.train.labels.astype(np.int32),
                          num_epochs=None,
                          batch_size=opts.batch_size,
-                         shuffle=True)
+                         shuffle=True,
+                         queue_capacity=4*opts.batch_size,
+                         num_threads=4)
     eval_input_fn = tf.estimator.inputs.numpy_input_fn(
                          x=data.test.images,
                          y=data.test.labels.astype(np.int32),
@@ -150,9 +162,9 @@ def main(opts):
             steps=opts.eval_steps // hvd.size(),
             hooks=[bcast_hook])
 
-        eval_results = estimator.evaluate(
+        estimator.evaluate(
             input_fn=eval_input_fn)
 
+
 if __name__ == '__main__':
-    opts = get_args()
-    main(opts)
+    main(get_args())
