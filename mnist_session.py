9f0fc853dd71de0178c4b6b57966a6ea1b6e7091
Adrian Yi <sjay87@gmail.com>
2018-10-25 19:42:49 -0700

    With no data files

--- mnist_session.py
+++ mnist_session.py
@@ -1,10 +1,11 @@
-import glob
 import os
 from argparse import ArgumentParser
 from functools import partial
 
+import numpy as np
 import tensorflow as tf
 from tensorflow.python.client.device_lib import list_local_devices
+from tensorflow.examples.tutorials.mnist.input_data import read_data_sets
 from clusterone import get_data_path, get_logs_path
 
 tf.logging.set_verbosity(tf.logging.INFO)
@@ -57,15 +58,15 @@ def get_args():
     parser.add_argument('--eval_secs', type=int, default=120,
                         help='throttle_secs for EvalSpec')
 
-    opts = parser.parse_args()
+    args = parser.parse_args()
 
-    opts.data_dir = get_data_path(dataset_name = 'adrianyi/mnist-data',
-                                 local_root = opts.local_data_dir,
-                                 local_repo = '',
-                                 path = '')
-    opts.log_dir = get_logs_path(root = opts.local_log_dir)
+    args.data_dir = get_data_path(dataset_name='*/*',
+                                  local_root=args.local_data_dir,
+                                  local_repo='',
+                                  path='')
+    args.log_dir = get_logs_path(root=args.local_log_dir)
 
-    return opts
+    return args
 
 
 def device_and_target():
@@ -73,10 +74,10 @@ def device_and_target():
     # Don't set a device.
     if job_name is None:
         print("Running single-machine training")
-        return (None, "")
+        return None, ""
 
     # Otherwise we're running distributed TensorFlow.
-    print("%s.%d  -- Running distributed training"%(job_name, task_index))
+    print("%s.%d  -- Running distributed training" % (job_name, task_index))
     if task_index is None or task_index == "":
         raise ValueError("Must specify an explicit `task_index`")
     if ps_hosts is None or ps_hosts == "":
@@ -104,41 +105,65 @@ def device_and_target():
     )
 
 
-def parser(example):
-    features = {'image': tf.FixedLenFeature([784], dtype=tf.float32),
-                'label': tf.FixedLenFeature([], dtype=tf.int64)}
-    data = tf.parse_single_example(
-        serialized=example,
-        features=features)
-    return tf.cast(data['image'], tf.float32), tf.cast(data['label'], tf.int32)
+# def parser(example):
+#     features = {'image': tf.FixedLenFeature([784], dtype=tf.float32),
+#                 'label': tf.FixedLenFeature([], dtype=tf.int64)}
+#     data = tf.parse_single_example(
+#         serialized=example,
+#         features=features)
+#     return tf.cast(data['image'], tf.float32), tf.cast(data['label'], tf.int32)
+#
+#
+# def dataset_fn(filenames, batch_size=512, train=False):
+#     """Input function to be used for Estimator class"""
+#     dataset = tf.data.TFRecordDataset(filenames, num_parallel_reads=2)
+#     dataset = dataset.map(parser)
+#     if train:
+#         dataset = tf.data.Dataset.apply(dataset, tf.contrib.data.shuffle_and_repeat(5 * opts.batch_size, count=None))
+#     dataset = dataset.batch(batch_size=batch_size)
+#     return dataset
 
 
-def dataset_fn(filenames, batch_size=512, train=False):
+class IteratorInitializerHook(tf.train.SessionRunHook):
+    """From https://medium.com/onfido-tech/higher-level-apis-in-tensorflow-67bfb602e6c0"""
+    def __init__(self):
+        super(IteratorInitializerHook, self).__init__()
+        self.iterator_initializer_func = None
+
+    def after_create_session(self, session, coord):
+        self.iterator_initializer_func(session)
+
+
+def input_fn(x, y, batch_size=512, train=True):
     """Input function to be used for Estimator class"""
-    dataset = tf.data.TFRecordDataset(filenames, num_parallel_reads=2)
-    dataset = dataset.map(parser)
+    data_x = tf.data.Dataset.from_tensor_slices(x)
+    data_y = tf.data.Dataset.from_tensor_slices(y)
+    dataset = tf.data.Dataset.zip((data_x, data_y))
+
     if train:
-        dataset = tf.data.Dataset.apply(dataset, tf.contrib.data.shuffle_and_repeat(5 * opts.batch_size, count=None))
+        dataset = tf.data.Dataset.apply(dataset,
+                                        tf.contrib.data.shuffle_and_repeat(5 * batch_size, count=None))
     dataset = dataset.batch(batch_size=batch_size)
+
     return dataset
 
 
-def cnn_net(input_tensor):
+def cnn_net(input_tensor, opts):
     """Return logits output from CNN net"""
     temp = tf.reshape(input_tensor, shape=(-1, 28, 28, 1), name='input_image')
     for i, n_units in enumerate(opts.hidden_units):
         temp = tf.layers.conv2d(temp, filters=n_units, kernel_size=(3, 3), strides=(2, 2),
                                 activation=tf.nn.relu, name='cnn'+str(i))
         temp = tf.layers.dropout(temp, rate=opts.dropout)
-    temp = tf.reduce_mean(temp, axis=(2,3), keepdims=False, name='average')
+    temp = tf.reduce_mean(temp, axis=(2, 3), keepdims=False, name='average')
     return tf.layers.dense(temp, 10)
 
 
-def tower_fn(data):
+def tower_fn(data, opts):
     features, labels = data[0], data[1]
     # logits = distribution.call_for_each_tower(cnn_net, features)
     # logits = tf.group(distribution.unwrap(logits))
-    logits = cnn_net(features)
+    logits = cnn_net(features, opts)
     pred = tf.cast(tf.argmax(logits, axis=1), tf.int64)
     acc, acc_op = tf.metrics.accuracy(labels, pred)
 
@@ -153,34 +178,52 @@ def tower_fn(data):
     # return pred, loss, train_op
     return train_op
 
+
 def main(opts):
     device, target = device_and_target()
     print('Device:', device)
     print('Target:', target)
 
-    filenames = glob.glob(os.path.join(opts.data_dir, 'train*.tfrecords'))
-    train_dataset_fn = partial(dataset_fn, filenames, batch_size=opts.batch_size, train=True)
+    # filenames = glob.glob(os.path.join(opts.data_dir, 'train*.tfrecords'))
+    # train_dataset_fn = partial(dataset_fn, filenames, batch_size=opts.batch_size, train=True)
+
+    data = read_data_sets(opts.data_dir,
+                          one_hot=False,
+                          fake_data=False)
+
+    train_x_ph = tf.placeholder(tf.float32, [None, 784])
+    train_y_ph = tf.placeholder(tf.int32, [None])
+
+    train_dataset_fn = partial(input_fn, train_x_ph, train_y_ph, batch_size=opts.batch_size, train=True)
+
+    # train_input_fn, train_iter_hook = get_inputs(data.train.images,
+    #     tf.keras.utils.to_categorical(data.train.labels.astype(np.int32), 10).astype(np.float32),
+    #     batch_size=opts.batch_size,
+    #     train=True)
+    # eval_input_fn, eval_iter_hook = get_inputs(data.test.images,
+    #     tf.keras.utils.to_categorical(data.test.labels.astype(np.int32), 10).astype(np.float32),
+    #     batch_size=opts.batch_size,
+    #     train=False)
 
     with distribution.scope():
-        iterator = distribution.distribute_dataset(train_dataset_fn).make_one_shot_iterator()
-        # pred, loss, train_op = tower_fn(iterator.get_next())
-        tower_train_ops = distribution.call_for_each_tower(tower_fn, iterator.get_next())
-        unwrapped = distribution.unwrap(tower_train_ops)
-        train_op = tf.group(unwrapped)
+        iterator = distribution.distribute_dataset(train_dataset_fn).make_initializable_iterator()
+        tower_train_ops = distribution.call_for_each_tower(tower_fn, iterator.get_next(), opts)
+        train_op = tf.group(distribution.unwrap(tower_train_ops))
 
     with tf.train.MonitoredTrainingSession(
              master=target,
              is_chief=(task_index == 0),
              checkpoint_dir=opts.log_dir,
              log_step_count_steps=50) as sess:
+        sess.run(iterator.initializer, feed_dict={train_x_ph: data.train.images,
+                                                  train_y_ph: data.train.labels.astype(np.int32)})
         local_step = 0
         while not sess.should_stop():
             local_step += 1
             loss_value, _, global_step = sess.run(['loss:0', train_op, tf.train.get_or_create_global_step()])
-            if local_step%10 == 0:
+            if local_step % 50 == 0:
                 tf.logging.info('{} {} {} {}'.format(task_index, local_step, global_step, loss_value))
 
 
 if __name__ == '__main__':
-    opts = get_args()
-    main(opts)
+    main(get_args())
