ef22cf76802ffa4d6a78a788f382c53361f88a79
Adrian Yi <sjay87@gmail.com>
2018-11-01 02:17:47 -0700

    Script to generate ssh-key

--- run_horovod.py
+++ run_horovod.py
@@ -1,6 +1,7 @@
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 import os
 import subprocess
+import time
 
 try:
     job_name = os.environ['JOB_NAME']
@@ -17,6 +18,8 @@ print('task_index:', task_index)
 print('ps_hosts:', ps_hosts)
 print('worker_hosts:', worker_hosts)
 
+is_chief = job_name == 'worker' and task_index == 0
+
 print('='*40, 'Environment Variables', '='*40)
 for k, v in os.environ.items():
     print('{}: {}'.format(k, v))
@@ -32,6 +35,34 @@ def parse_args():
     return opts, opts2
 
 
+def make_ssh_or_wait():
+    if is_chief:
+        print('Generating ssh-key & config')
+        cmd = ' && '.join([
+            'mkdir /data/.ssh/',
+            'ssh-keygen -f /data/.ssh/id_rsa -t rsa -N ""',
+            'mv /data/.ssh/id_rsa.pub /data/.ssh/authorized_keys',
+            'chmod 600 /data/.ssh/authorized_keys',
+            'echo "Host *" >> /data/.ssh/config',
+            'echo " IdentityFile ~/.ssh/id_rsa" >> /data/.ssh/config',
+            'cp -r /data/.ssh ~/.ssh'
+        ])
+        print('Running command: {}'.format(cmd))
+        subprocess.call(cmd, shell=True)
+        time.sleep(5)
+    else:
+        print('Waiting for chief node to create ssh keys')
+        while not os.path.exists('/data/.ssh/id_rsa'):
+            time.sleep(3)
+        time.sleep(1)
+        cmd = ' && '.join([
+            'cp -r /data/.ssh ~/.ssh',
+            '/usr/sbin/sshd -p 5000'
+        ])
+        print('Running command: {}'.format(cmd))
+        subprocess.call(cmd, shell=True)
+
+
 def main(opts, opts2):
     proc_per_device = max(opts.gpus_per_device, 1)
     hosts = [host.split(':')[0] for host in worker_hosts+ps_hosts]
@@ -40,14 +71,14 @@ def main(opts, opts2):
     n_procs = proc_per_device * n_hosts
     hosts = ','.join([host+':{}'.format(proc_per_device) for host in hosts])
 
-    if job_name == 'worker' and task_index == 0:
+    if is_chief:
         # From https://github.com/uber/horovod/blob/master/docs/docker.md
         # mpirun -np 16 -H host1:4,host2:4,host3:4,host4:4 -mca plm_rsh_args "-p 12345" python keras_mnist_advanced.py
         cmd = 'mpirun --verbose -np {np} -H {hosts} -mca plm_rsh_args "-p {port}" python -m {module}'\
               .format(np=n_procs, hosts=hosts, port=port, module=opts.module)
         cmd = ' '.join([cmd]+opts2)
     else:
-        cmd = 'bash -c "/usr/sbin/sshd -p {port}; sleep infinity'.format(port=port)
+        cmd = 'sleep infinity'.format(port=port)
 
     print('-'*50)
     print('Running command: {}'.format(cmd))
@@ -56,4 +87,5 @@ def main(opts, opts2):
 
 
 if __name__ == '__main__':
+    make_ssh_or_wait()
     main(*parse_args())
